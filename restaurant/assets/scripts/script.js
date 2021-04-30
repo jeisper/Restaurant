b3c22dc441566c2b8da390e384dba5960d0e62b1
jeisper <71713717+jeisper@users.noreply.github.com>
2021-04-30 23:34:41 +0100

    almost broke

--- restaurant/assets/scripts/script.js
+++ restaurant/assets/scripts/script.js
@@ -3,80 +3,89 @@ let isLoggedIn = true;
 const users = [];
 
 const menu = {
- starters = [{
-    name = "Ceaser Salada",
-    desc = "Lettuce with parmesan cheese and cesar dressing",
-    veg = true,
-    cost = 8
-  },
-  {
-    name = "Fries",
-    desc = "Portion of chips",
-    veg = true,
-    cost = 4
-  },
-  {
-    name = "Chicken Wings",
-    desc = "Portion of spicy chicken wings with blue cheese sauce",
-    veg = false,
-    cost = 12
-  }
-],
-  mains = [{
-    name = "Fish and Chips",
-    desc = "A fried code followed with chips and tartare sauce",
-    veg = false,
-    cost = 15
-  },
-  {
-    name = "Beef Stew",
-    desc = "Portion of beef stew followed with a portion of mash potatos",
-    veg = false,
-    cost = 14
-  },
-  {
-    name = "Vegetable Pie",
-    desc = "A vegetable pie followed with a potion of mash potato and tomato sauce",
-    veg = true,
-    cost = 12
-  }],
-  deserts = [{
-    name = "Lemon CheeseCake",
-    desc = "A piece of Lemon CheeseCake Topped with Fresh Cream",
-    veg = true,
-    cost = 6
-  },
-  {
-    name = "Vanilla Ice Cream",
-    desc = "3 scoops of Vanilla ice cream with chocolate or caramel topping",
-    veg = true,
-    cost = 5
-  },
-  {
-    name = "Chocolate Brownie",
-    desc = "2 pieces of chocolate brownie followed with a scoop of vanilla ice cream",
-    veg = true,
-    cost = 7
-  }],
-  drinks = [{
-    name = "Orange Juice",
-    desc = "300ml glass of fresh orange juice",
-    veg = true,
-    cost = 4
-  },
-  {
-    name = "Coke",
-    desc = "300ml glass of Coke followed with Ice and lime",
-    veg = true,
-    cost = 3
-  },
-  {
-    name = "Beer",
-    desc = "500ml pint of Beer",
-    veg = true,
-    cost = 5
-  }]
-}
+  starters: [
+    {
+      name: "Ceaser Salada",
+      desc: "Lettuce with parmesan cheese and cesar dressing.",
+      veg: true,
+      cost: 8,
+    },
+    {
+      name: "Fries",
+      desc: "Portion of chips.",
+      veg: true,
+      cost: 4,
+    },
+    {
+      name: "Chicken Wings",
+      desc: "Portion of spicy chicken wings with blue cheese sauce.",
+      veg: false,
+      cost: 12,
+    },
+  ],
+  mains: [
+    {
+      name: "Fish and Chips",
+      desc: "A fried code followed with chips and tartare sauce.",
+      veg: false,
+      cost: 15,
+    },
+    {
+      name: "Beef Stew",
+      desc: "Portion of beef stew followed with a portion of mash potatos.",
+      veg: false,
+      cost: 14,
+    },
+    {
+      name: "Vegetable Pie",
+      desc:
+        "A vegetable pie followed with a potion of mash potato and tomato sauce.",
+      veg: true,
+      cost: 12,
+    },
+  ],
+  desserts: [
+    {
+      name: "Lemon CheeseCake",
+      desc: "A piece of Lemon CheeseCake Topped with Fresh Cream.",
+      veg: true,
+      cost: 6,
+    },
+    {
+      name: "Vanilla Ice Cream",
+      desc: "3 scoops of Vanilla ice cream with chocolate or caramel topping.",
+      veg: true,
+      cost: 5,
+    },
+    {
+      name: "Chocolate Brownie",
+      desc:
+        "2 pieces of chocolate brownie followed with a scoop of vanilla ice cream.",
+      veg: true,
+      cost: 7,
+    },
+  ],
+  drinks: [
+    {
+      name: "Orange Juice",
+      desc: "300ml glass of fresh orange juice.",
+      veg: true,
+      cost: 4,
+    },
+    {
+      name: "Coke",
+      desc: "300ml glass of Coke followed with Ice and lime.",
+      veg: true,
+      cost: 3,
+    },
+    {
+      name: "Beer",
+      desc: "500ml pint of Beer.",
+      veg: true,
+      cost: 5,
+    },
+  ],
+};
 
 $(document).ready(function () {
   runJS();
@@ -85,6 +94,7 @@ $(document).ready(function () {
 function runJS() {
   $(".dashboard").hide();
   updateViews();
+  updateMenu();
 
   $("#login_form").submit(function (e) {
     e.preventDefault();
@@ -118,10 +128,58 @@ function runJS() {
       },
     });
   }
-
+  function updateMenu() {
+    $(".starters").html("");
+    $(".mains").html("");
+    $(".desserts").html("");
+    $(".drinks").html("");
+    for (let i = 0; i < menu.starters.length; i++) {
+      $(".starters").append(` 
+      <label>
+        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <b>${menu.starters[i].name + " - €" + menu.starters[i].cost} </b>
+        <p>
+          ${menu.starters[i].desc}
+        </p>
+      </label>
+    `);
+    }
+    for (let i = 0; i < menu.mains.length; i++) {
+      $(".mains").append(` 
+      <label>
+        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <b>${menu.mains[i].name + " - €" + menu.mains[i].cost} </b>
+        <p>
+          ${menu.mains[i].desc}
+        </p>
+      </label>
+    `);
+    }
+    for (let i = 0; i < menu.desserts.length; i++) {
+      $(".desserts").append(` 
+      <label>
+        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <b>${menu.desserts[i].name + " - €" + menu.desserts[i].cost} </b>
+        <p>
+          ${menu.desserts[i].desc}
+        </p>
+      </label>
+    `);
+    }
+    for (let i = 0; i < menu.drinks.length; i++) {
+      $(".drinks").append(` 
+      <label>
+        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <b>${menu.drinks[i].name + " - €" + menu.drinks[i].cost} </b>
+        <p>
+          ${menu.drinks[i].desc}
+        </p>
+      </label>
+    `);
+    }
+  }
   function updateusers() {
-    let text = "";
-    $(".customer_list").html();
+    $(".customer_list").html("");
     for (let i = 0; i < users.length; i++) {
       let user = users[i];
       let name = user.name.first + " " + user.name.last;
