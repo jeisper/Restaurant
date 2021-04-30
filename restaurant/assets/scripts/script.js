4d963dcbb2d3f47417cb72bc5954c5bf8239c85f
jeisper <71713717+jeisper@users.noreply.github.com>
2021-04-30 18:21:26 +0100

    save

--- restaurant/assets/scripts/script.js
+++ restaurant/assets/scripts/script.js
@@ -1,4 +1,6 @@
-let isLoggedIn = false;
+let isLoggedIn = true;
+
+const Users = [];
 
 $(document).ready(function () {
   runJS();
@@ -20,12 +22,34 @@ function runJS() {
     if (isLoggedIn) {
       $(".dashboard").show();
       $(".login").hide();
+      for (let i = 0; i < 5; i++) {
+        fatchAPI();
+      }
     } else {
       $(".dashboard").hide();
       $(".login").show();
     }
   }
+  function fatchAPI() {
+    $.ajax({
+      url: "https://randomuser.me/api/",
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        Users.push(data.results[0].name.first);
+        console.log(data.results[0].name.first);
+        updateUsers();
+      },
+    });
+  }
 
+  function updateUsers() {
+    let text = "";
+    for (let i = 0; i < Users.length; i++) {
+      text += Users[i] + " ";
+    }
+    $(".customer").text(text);
+  }
   function validatePassword(password) {
     let strongRegex = new RegExp(
       "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
