6db15db29a3f0559f52111a7a65a65a47a39c0a9
Harsh Chandra <harshchandrahc5@gmail.com>
2021-05-01 22:07:17 +0100

    Almost working

--- restaurant/assets/scripts/script.js
+++ restaurant/assets/scripts/script.js
@@ -1,6 +1,7 @@
 let isLoggedIn = true;
 
 const users = [];
+const bill = {};
 
 const menu = {
   starters: [
@@ -124,14 +125,13 @@ function runJS() {
       success: function (data) {
         users.push(data.results[0]);
         updateusers();
-        console.log(data);
       },
     });
   }
 
-  function createDropdown() {
+  function createDropdown(id) {
     let html = `
-    <select name="quantity">
+    <select name="quantity" id="${id}">
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -148,6 +148,16 @@ function runJS() {
     return html;
   }
 
+  function selectItem(id, itemType, item, quantity) {
+    const bill_item = { ...item };
+    bill_item.type = itemType;
+    bill_item.quantity = quantity;
+
+    bill[id] = bill_item;
+
+    console.log(bill);
+  }
+
   function updateMenu() {
     $(".starters").html("");
     $(".mains").html("");
@@ -156,50 +166,146 @@ function runJS() {
     for (let i = 0; i < menu.starters.length; i++) {
       $(".starters").append(` 
       <label>
-        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <input type="checkbox" name="starter1" id="${
+          menu.starters[i].name.toLowerCase().replace(" ", "_") + "_label"
+        }" />
         <b>${menu.starters[i].name + " - €" + menu.starters[i].cost} </b>
         <p>
           ${menu.starters[i].desc}
         </p>
-        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown()}
+        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown(
+          menu.starters[i].name.toLowerCase().replace(" ", "_")
+        )}
       </label>
     `);
+
+      $(
+        "#" + menu.starters[i].name.toLowerCase().replace(" ", "_") + "_label"
+      ).change(function () {
+        if (this.checked) {
+          selectItem(
+            menu.starters[i].name.toLowerCase().replace(" ", "_"),
+            "starters",
+            menu.starters[i],
+            $("#" + menu.starters[i].name.toLowerCase().replace(" ", "_")).val()
+          );
+        } else {
+          selectItem(
+            menu.starters[i].name.toLowerCase().replace(" ", "_"),
+            "starters",
+            menu.starters[i],
+            0
+          );
+        }
+      });
     }
     for (let i = 0; i < menu.mains.length; i++) {
       $(".mains").append(` 
       <label>
-        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <input type="checkbox" name="starter1" id="${
+          menu.mains[i].name.toLowerCase().replace(" ", "_") + "_label"
+        }"/>
         <b>${menu.mains[i].name + " - €" + menu.mains[i].cost} </b>
         <p>
           ${menu.mains[i].desc}
         </p>
-        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown()}
+        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown(
+          menu.mains[i].name.toLowerCase().replace(" ", "_")
+        )}
       </label>
     `);
+
+      $(
+        "#" + menu.mains[i].name.toLowerCase().replace(" ", "_") + "_label"
+      ).change(function () {
+        if (this.checked) {
+          selectItem(
+            menu.mains[i].name.toLowerCase().replace(" ", "_"),
+            "mains",
+            menu.mains[i],
+            $("#" + menu.mains[i].name.toLowerCase().replace(" ", "_")).val()
+          );
+        } else {
+          selectItem(
+            menu.mains[i].name.toLowerCase().replace(" ", "_"),
+            "starters",
+            menu.mains[i],
+            0
+          );
+        }
+      });
     }
     for (let i = 0; i < menu.desserts.length; i++) {
       $(".desserts").append(` 
       <label>
-        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <input type="checkbox" name="starter1" id="${
+          menu.desserts[i].name.toLowerCase().replace(" ", "_") + "_label"
+        }"/>
         <b>${menu.desserts[i].name + " - €" + menu.desserts[i].cost} </b>
         <p>
           ${menu.desserts[i].desc}
         </p>
-        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown()}
+        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown(
+          menu.desserts[i].name.toLowerCase().replace(" ", "_")
+        )}
       </label>
     `);
+
+      $(
+        "#" + menu.desserts[i].name.toLowerCase().replace(" ", "_") + "_label"
+      ).change(function () {
+        if (this.checked) {
+          selectItem(
+            menu.desserts[i].name.toLowerCase().replace(" ", "_"),
+            "mains",
+            menu.desserts[i],
+            $("#" + menu.desserts[i].name.toLowerCase().replace(" ", "_")).val()
+          );
+        } else {
+          selectItem(
+            menu.desserts[i].name.toLowerCase().replace(" ", "_"),
+            "desserts",
+            menu.desserts[i],
+            0
+          );
+        }
+      });
     }
     for (let i = 0; i < menu.drinks.length; i++) {
       $(".drinks").append(` 
       <label>
-        <input type="checkbox" name="starter1" value="Ceaser_Salada" />
+        <input type="checkbox" name="starter1" id="${
+          menu.drinks[i].name.toLowerCase().replace(" ", "_") + "_label"
+        }"/>
         <b>${menu.drinks[i].name + " - €" + menu.drinks[i].cost} </b>
         <p>
           ${menu.drinks[i].desc}
         </p>
-        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown()}
+        <b style="font-size: 0.9rem">Quantity: <b/> ${createDropdown(
+          menu.drinks[i].name.toLowerCase().replace(" ", "_")
+        )}
       </label>
     `);
+
+      $(
+        "#" + menu.drinks[i].name.toLowerCase().replace(" ", "_") + "_label"
+      ).change(function () {
+        if (this.checked) {
+          selectItem(
+            menu.drinks[i].name.toLowerCase().replace(" ", "_"),
+            "drinks",
+            menu.drinks[i],
+            $("#" + menu.drinks[i].name.toLowerCase().replace(" ", "_")).val()
+          );
+        } else {
+          selectItem(
+            menu.drinks[i].name.toLowerCase().replace(" ", "_"),
+            "starters",
+            menu.drinks[i],
+            0
+          );
+        }
+      });
     }
   }
   function updateusers() {
